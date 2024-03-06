cbe47175602a6cb15e728a45320faee383566725
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-03-06 11:44:12 +0900

    web-service update

--- SPR/src/main/java/sprproject/SprApplication.java
+++ SPR/src/main/java/sprproject/SprApplication.java
@@ -11,5 +11,4 @@ public class SprApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SprApplication.class, args);
 	}
-
 }
---
+++ SPR/src/main/java/sprproject/SpringConfig.java
@@ -0,0 +1,7 @@
+package sprproject;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+}
