78e4c26388fb8197d90df52d787eeabd9c3fa07b
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-03-07 15:51:09 +0900

    product update

--- item-service/item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ item-service/item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -1,9 +1,14 @@
 package hello.itemservice.domain.item;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemRepositoryTest {
@@ -18,10 +23,43 @@ class ItemRepositoryTest {
     @Test
     void save(){
         //given
-        
-        //when
+        Item item= new Item("itema", 10000, 10);
 
-        //them
+        //when
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(item);
+    }
+
+    @Test
+    @DisplayName("findAllItem")
+    void findAll(){
+        Item item1 = new Item("item1", 100000, 1);
+        Item item2 = new Item("item2", 200000, 2);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        List<Item> result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+
+    }
+
+    @Test
+    @DisplayName("제품 수정")
+    void updateItem(){
+        Item item1 = new Item("ITEM", 10000, 1);
+        Item updateItem = new Item("updateIT", 101010, 1);
+
+        itemRepository.save(item1);
+        itemRepository.update(item1.getId(), updateItem);
+
+        assertThat(item1.getItemName()).isEqualTo(updateItem.getItemName());
+
     }
 
 }
